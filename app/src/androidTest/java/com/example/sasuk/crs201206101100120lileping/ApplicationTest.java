fdf717054a608ceccf67c2ec92b5efcd797554ad
Erencgsx <sasukecgsx@hotmail.com>
2015-12-24 14:45:25 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/sasuk/crs201206101100120lileping/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.sasuk.crs201206101100120lileping;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
